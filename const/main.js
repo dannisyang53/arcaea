ecdb4aac441c5c7c0d584434243de5f460acf1db
Dannis Yang <spb035@outlook.com>
2024-12-09 18:15:16 +0800

    Formations & Modifications

--- const/main.js
+++ const/main.js
@@ -1,78 +1,114 @@
 // show/hide notes
 const declutterButton = document.getElementById("input-declutter");
-declutterButton.addEventListener('change', declutterNotes);
+declutterButton.addEventListener("change", declutterNotes);
 
 function declutterNotes(e) {
-    console.log(e);
-    const notesAll = document.getElementsByClassName('note');
-    for (const noteEl of notesAll) {
-        noteEl.classList.toggle("hidden");
-    }
+  console.log(e);
+  const notesAll = document.getElementsByClassName("note");
+  for (const noteEl of notesAll) {
+    noteEl.classList.toggle("hidden");
+  }
 }
 
 window.addEventListener("load", declutterNotes);
 
-
 // generate radio groups for chart level
-const lvCCs = ['1', '2', '3', '4', '5', '6', '7', '7.7', '8', '8.7', '9', '9.7', '10', '10.7', '11', '11.8', '12'];
+const lvCCs = [
+  "1",
+  "2",
+  "3",
+  "4",
+  "5",
+  "6",
+  "7",
+  "7.7",
+  "8",
+  "8.7",
+  "9",
+  "9.7",
+  "10",
+  "10.7",
+  "11",
+  "11.7",
+  "12",
+];
 
 const minScoreDiv = document.getElementById("input-lv");
-minScoreDiv.innerHTML = '\n\t' + lvCCs.map((lvcc) => `<div class="rb">\n` + `\t\t<label><input type="radio" name="lvcc" value="${lvcc}">${ccToLv(lvcc)}</label>\n\t</div>`).join('\n\t') + '\n';
+minScoreDiv.innerHTML =
+  "\n\t" +
+  lvCCs
+    .map(
+      (lvcc) =>
+        `<div class="rb">\n` +
+        `\t\t<label><input type="radio" name="lvcc" value="${lvcc}">${ccToLv(
+          lvcc
+        )}</label>\n\t</div>`
+    )
+    .join("\n\t") +
+  "\n";
 
 // event listener for chart level change event
 const radioButtonsChart = document.querySelectorAll('input[name="lvcc"]');
 for (const radioButton of radioButtonsChart) {
-    radioButton.addEventListener('change', updateMinScore);
+  radioButton.addEventListener("change", updateMinScore);
 }
 
 function ccToLv(cc) {
-    if (cc.includes('.')) {
-        let lvDp = parseInt(cc.split('.')[0]);
-        if (lvDp >= 7) {
-            return parseInt(cc) + '+';
-        } else {
-            return cc;
-        }
+  if (cc.includes(".")) {
+    let lvDp = parseInt(cc.split(".")[0]);
+    if (lvDp >= 7) {
+      return parseInt(cc) + "+";
     } else {
-        return cc;
+      return cc;
     }
+  } else {
+    return cc;
+  }
 }
 
 function ccToMinScore(cc) {
-    let sm = -parseFloat(cc);
-    let sc;
-    if (sm >= 2) {
-        sc = 10000000;
-    } else if (sm >= 1) {
-        sc = (sm - 1) * 200000 + 9800000;
-    } else {
-        sc = sm * 300000 + 9500000;
-    }
-    return parseInt(sc);
+  let sm = -parseFloat(cc);
+  let sc;
+  if (sm >= 2) {
+    sc = 10000000;
+  } else if (sm >= 1) {
+    sc = (sm - 1) * 200000 + 9800000;
+  } else {
+    sc = sm * 300000 + 9500000;
+  }
+  return parseInt(sc);
 }
 
 function scoreToScoreMod(sc) {
-    if (sc >= 10000000) {
-        return 2;
-    } else if (sc > 9800000) {
-        return 1 + (sc - 9800000) / 200000;
-    } else {
-        return (sc - 9500000) / 300000;
-    }
+  if (sc >= 10000000) {
+    return 2;
+  } else if (sc > 9800000) {
+    return 1 + (sc - 9800000) / 200000;
+  } else {
+    return (sc - 9500000) / 300000;
+  }
 }
 
 function scoreDisp(score) {
-    let mill = Math.floor(score / 1000000);
-    let thou = Math.floor((score % 1000000) / 1000);
-    let ones = score % 1000;
-    return mill + "'" + String(thou).padStart(3, '0') + "'" + String(ones).padStart(3, '0');
+  let mill = Math.floor(score / 1000000);
+  let thou = Math.floor((score % 1000000) / 1000);
+  let ones = score % 1000;
+  return (
+    mill +
+    "'" +
+    String(thou).padStart(3, "0") +
+    "'" +
+    String(ones).padStart(3, "0")
+  );
 }
 
 function updateMinScore(e) {
-    console.log(e);
-    if (this.checked) {
-        document.getElementById('minscorereq').innerText = `${scoreDisp(ccToMinScore(this.value))}`;
-    }
+  console.log(e);
+  if (this.checked) {
+    document.getElementById("minscorereq").innerText = `${scoreDisp(
+      ccToMinScore(this.value)
+    )}`;
+  }
 }
 
 const minCcEl = document.getElementById("ccmin");
@@ -96,68 +132,82 @@ const errStpUSpecial = 0;
 const errStpUSealed = 0;
 
 function updateStpUError(e) {
-    console.log(e);
-    if (partnerStepInput.value >= 160) {
-        errStpUInput.setAttribute('value', errStpUSpecial);
-    } else if (partnerLvInput.checked && !partnerNonmaxInput.checked) {
-        errStpUInput.setAttribute('value', errStpUHighLv);
-    } else if (partnerStepInput.value === 50) {
-        errStpUInput.setAttribute('value', errStpUSealed);
-    } else {
-        errStpUInput.setAttribute('value', errStpUDefault);
-    }
+  console.log(e);
+  if (partnerStepInput.value >= 160) {
+    errStpUInput.setAttribute("value", errStpUSpecial);
+  } else if (partnerLvInput.checked && !partnerNonmaxInput.checked) {
+    errStpUInput.setAttribute("value", errStpUHighLv);
+  } else if (partnerStepInput.value === 50) {
+    errStpUInput.setAttribute("value", errStpUSealed);
+  } else {
+    errStpUInput.setAttribute("value", errStpUDefault);
+  }
 }
 
 // event listener to calculate CCs on button press
 function updateCCs() {
-    var stepstat = parseInt(document.getElementById("input-partnerstep").value);
-    var stamboost = parseInt(document.querySelector('input[name="stam"]:checked').value);
-    var fragboost = parseFloat(document.querySelector('input[name="frag"]:checked').value);
-    var worldboost = parseFloat(document.querySelector('input[name="world"]:checked').value);
-    var mmrboost = parseInt(document.querySelector('input[name="mmr"]:checked').value)
-    var score = parseInt(document.getElementById("input-score").value);
-    var mapprog = parseFloat(document.getElementById("input-mapprog").value);
-    var errmpl = parseFloat(document.getElementById("input-errmpl").value);
-    var errmpu = parseFloat(document.getElementById("input-errmpu").value);
-    var errstpl = parseFloat(document.getElementById("input-errstpl").value);
-    var errstpu = parseFloat(document.getElementById("input-errstpu").value);
+  var stepstat = parseInt(document.getElementById("input-partnerstep").value);
+  var stamboost = parseInt(
+    document.querySelector('input[name="stam"]:checked').value
+  );
+  var fragboost = parseFloat(
+    document.querySelector('input[name="frag"]:checked').value
+  );
+  var worldboost = parseFloat(
+    document.querySelector('input[name="world"]:checked').value
+  );
+  var mmrboost = parseInt(
+    document.querySelector('input[name="mmr"]:checked').value
+  );
+  var score = parseInt(document.getElementById("input-score").value);
+  var mapprog = parseFloat(document.getElementById("input-mapprog").value);
+  var errmpl = parseFloat(document.getElementById("input-errmpl").value);
+  var errmpu = parseFloat(document.getElementById("input-errmpu").value);
+  var errstpl = parseFloat(document.getElementById("input-errstpl").value);
+  var errstpu = parseFloat(document.getElementById("input-errstpu").value);
 
-    let worldmult = stamboost * fragboost * worldboost * mmrboost;
-    let mincc = calcCC(mapprog - errmpl, score + 1, stepstat + errstpu, worldmult);
-    let maxcc = calcCC(mapprog + errmpu, score, stepstat - errstpl, worldmult);
-    minCcEl.innerText = mincc.toFixed(3);
-    maxCcEl.innerText = (maxcc + 0.001).toFixed(3);
+  let worldmult = stamboost * fragboost * worldboost * mmrboost;
+  let mincc = calcCC(
+    mapprog - errmpl,
+    score + 1,
+    stepstat + errstpu,
+    worldmult
+  );
+  let maxcc = calcCC(mapprog + errmpu, score, stepstat - errstpl, worldmult);
+  minCcEl.innerText = mincc.toFixed(3);
+  maxCcEl.innerText = (maxcc + 0.001).toFixed(3);
 
-    let minccrnd;
-    let maxccrnd;
-    if (mincc < 7.5) {
-        minccrnd = Math.ceil(mincc * 2) / 2;
-    } else if (mincc < 8) {
-        minccrnd = 7.8;
-    } else {
-        minccrnd = Math.ceil(mincc * 10) / 10;
-    }
-    if (maxcc < 7.8) {
-        maxccrnd = Math.floor(maxcc * 2) / 2;
-    } else if (maxcc < 8) {
-        maxccrnd = 7.8
-    } else {
-        maxccrnd = Math.floor(maxcc * 10) / 10;
-    }
-    minCcRndEl.innerText = minccrnd.toFixed(1);
-    maxCcRndEl.innerText = maxccrnd.toFixed(1);
+  let minccrnd;
+  let maxccrnd;
+  if (mincc < 7.5) {
+    minccrnd = Math.ceil(mincc * 2) / 2;
+  } else if (mincc < 8) {
+    minccrnd = 7.8;
+  } else {
+    minccrnd = Math.ceil(mincc * 10) / 10;
+  }
+  if (maxcc < 7.8) {
+    maxccrnd = Math.floor(maxcc * 2) / 2;
+  } else if (maxcc < 8) {
+    maxccrnd = 7.8;
+  } else {
+    maxccrnd = Math.floor(maxcc * 10) / 10;
+  }
+  minCcRndEl.innerText = minccrnd.toFixed(1);
+  maxCcRndEl.innerText = maxccrnd.toFixed(1);
 
-    if (minccrnd > maxccrnd) {
-        errMsg.innerText = "最小或最大估计值无效，请检查您的输入。";
-    } else if (minccrnd < maxccrnd) {
-        errMsg.innerText = "下限和上限之间的差值太大，无法确定定数。使用其他搭档或较低的分数重试（勾选上方的复选框以获取更多信息）。";
-    } else {
-        errMsg.innerText = "";
-    }
+  if (minccrnd > maxccrnd) {
+    errMsg.innerText = "最小或最大估计值无效，请检查您的输入。";
+  } else if (minccrnd < maxccrnd) {
+    errMsg.innerText =
+      "下限和上限之间的差值太大，无法确定定数。使用其他搭档或较低的分数重试（勾选上方的复选框以获取更多信息）。";
+  } else {
+    errMsg.innerText = "";
+  }
 }
 
 function calcCC(mapprog, score, stepstat, worldmult) {
-    let stepbase = mapprog / worldmult / stepstat * 50;
-    let playrating = ((stepbase - 2.5) / 2.45) ** 2;
-    return playrating - scoreToScoreMod(score);
+  let stepbase = (mapprog / worldmult / stepstat) * 50;
+  let playrating = ((stepbase - 2.5) / 2.45) ** 2;
+  return playrating - scoreToScoreMod(score);
 }
--- steps/main.js
+++ steps/main.js
@@ -1,50 +1,67 @@
 // lvs and ccs
-const lvCCs = [['1', ['1.0', '1.5']], ['2', ['2.0', '2.5']], ['3', ['3.0', '3.5']], ['4', ['4.0', '4.5']], ['5', ['5.0', '5.5']], ['6', ['6.0', '6.5']], ['7', ['7.0', '7.5']], ['7+', ['7.8']], ['8', ['8.0', '8.1', '8.2', '8.3', '8.4', '8.5', '8.6']], ['8+', ['8.7', '8.8', '8.9']], ['9', ['9.0', '9.1', '9.2', '9.3', '9.4', '9.5', '9.6']], ['9+', ['9.7', '9.8', '9.9']], ['10', ['10.0', '10.1', '10.2', '10.3', '10.4', '10.5', '10.6']], ['10+', ['10.7', '10.8', '10.9']], ['11', ['11.0', '11.1', '11.2', '11.3', '11.4', '11.5', '11.6', '11.7']], ['11+', ['11.9']], ['12', ['12.0']]];
+const lvCCs = [
+  ["1", ["1.0", "1.5"]],
+  ["2", ["2.0", "2.5"]],
+  ["3", ["3.0", "3.5"]],
+  ["4", ["4.0", "4.5"]],
+  ["5", ["5.0", "5.5"]],
+  ["6", ["6.0", "6.5"]],
+  ["7", ["7.0", "7.5"]],
+  ["7+", ["7.8"]],
+  ["8", ["8.0", "8.1", "8.2", "8.3", "8.4", "8.5", "8.6"]],
+  ["8+", ["8.7", "8.8", "8.9"]],
+  ["9", ["9.0", "9.1", "9.2", "9.3", "9.4", "9.5", "9.6"]],
+  ["9+", ["9.7", "9.8", "9.9"]],
+  ["10", ["10.0", "10.1", "10.2", "10.3", "10.4", "10.5", "10.6"]],
+  ["10+", ["10.7", "10.8", "10.9"]],
+  ["11", ["11.0", "11.1", "11.2", "11.3", "11.4", "11.5", "11.6"]],
+  ["11+", ["11.7", "11.8", "11.9"]],
+  ["12", ["12.0"]],
+];
 
 function scoreModToScore(sm) {
-    if (sm >= 1) {
-        return parseInt(9800000 + (sm - 1) * 200000);
-    } else {
-        return parseInt(9500000 + sm * 300000);
-    }
+  if (sm >= 1) {
+    return parseInt(9800000 + (sm - 1) * 200000);
+  } else {
+    return parseInt(9500000 + sm * 300000);
+  }
 }
 
 function scoreDisp(score) {
-    if (score >= 10000000) {
-        return "PM";
-    } else {
-        return score.toLocaleString('en-US').replaceAll(',', "'");
-    }
+  if (score >= 10000000) {
+    return "PM";
+  } else {
+    return score.toLocaleString("en-US").replaceAll(",", "'");
+  }
 }
 
 function scoreRange(goalmin, goalmax, stepstat, cc) {
-    let scoremin;
-    let scoremax = 0;
-    let prminsqrt = (goalmin * 50 / stepstat - 2.5) / 2.45;
-    if (prminsqrt < 0) {
-        scoremin = 0;
-    } else {
-        scoremin = scoreModToScore(prminsqrt ** 2 - cc);
-    }
-    let prmaxsqrt = (goalmax * 50 / (stepstat + 1) - 2.5) / 2.45;
-    console.log([goalmax, prmaxsqrt])
-    if (prmaxsqrt < 0) {
-        return false;
-    }
-    let smmax = prmaxsqrt ** 2 - cc;
-    if (smmax > 2) {
-        scoremax = 10000000;
-    } else {
-        scoremax = scoreModToScore(smmax);
-    }
-    console.log([scoremin, scoremax])
-    if (scoremin > scoremax) {
-        return false;
-    } else {
-        return [scoremin, scoremax];
-    }
+  let scoremin;
+  let scoremax = 0;
+  let prminsqrt = ((goalmin * 50) / stepstat - 2.5) / 2.45;
+  if (prminsqrt < 0) {
+    scoremin = 0;
+  } else {
+    scoremin = scoreModToScore(prminsqrt ** 2 - cc);
+  }
+  let prmaxsqrt = ((goalmax * 50) / (stepstat + 1) - 2.5) / 2.45;
+  console.log([goalmax, prmaxsqrt]);
+  if (prmaxsqrt < 0) {
+    return false;
+  }
+  let smmax = prmaxsqrt ** 2 - cc;
+  if (smmax > 2) {
+    scoremax = 10000000;
+  } else {
+    scoremax = scoreModToScore(smmax);
+  }
+  console.log([scoremin, scoremax]);
+  if (scoremin > scoremax) {
+    return false;
+  } else {
+    return [scoremin, scoremax];
+  }
 }
-
 
 // event listener to calculate score ranges on button press
 const scorerowscc = document.getElementById("scoresccrows");
@@ -52,100 +69,108 @@ const scorerowslv = document.getElementById("scoreslvrows");
 const errMsg = document.getElementById("calcerror");
 
 function calcScores() {
-    var goalmin = parseFloat(document.getElementById("input-goalmin").value);
-    var goalmax = parseFloat(document.getElementById("input-goalmax").value);
-    var ccmin = parseFloat(document.getElementById("input-ccmin").value);
-    var ccmax = parseFloat(document.getElementById("input-ccmax").value);
-    var stepstat = parseFloat(document.getElementById("input-partnerstep").value);
-    var stamboost = parseInt(document.querySelector('input[name="stam"]:checked').value);
-    var fragboost = parseFloat(document.querySelector('input[name="frag"]:checked').value);
-    var worldboost = parseFloat(document.querySelector('input[name="world"]:checked').value);
-    var mmrboost = parseInt(document.querySelector('input[name="mmr"]:checked').value)
-    goalmin = goalmin / (stamboost * fragboost * worldboost * mmrboost);
-    goalmax = goalmax / (stamboost * fragboost * worldboost * mmrboost);
+  var goalmin = parseFloat(document.getElementById("input-goalmin").value);
+  var goalmax = parseFloat(document.getElementById("input-goalmax").value);
+  var ccmin = parseFloat(document.getElementById("input-ccmin").value);
+  var ccmax = parseFloat(document.getElementById("input-ccmax").value);
+  var stepstat = parseFloat(document.getElementById("input-partnerstep").value);
+  var stamboost = parseInt(
+    document.querySelector('input[name="stam"]:checked').value
+  );
+  var fragboost = parseFloat(
+    document.querySelector('input[name="frag"]:checked').value
+  );
+  var worldboost = parseFloat(
+    document.querySelector('input[name="world"]:checked').value
+  );
+  var mmrboost = parseInt(
+    document.querySelector('input[name="mmr"]:checked').value
+  );
+  goalmin = goalmin / (stamboost * fragboost * worldboost * mmrboost);
+  goalmax = goalmax / (stamboost * fragboost * worldboost * mmrboost);
 
-    let srs = [];
-    for (let [lv, ccs] of lvCCs) {
-        let lvsrs = [];
-        for (let cc of ccs) {
-            sr = scoreRange(goalmin, goalmax, stepstat, cc);
-            if (cc < ccmin || cc > ccmax) {
-
-            } else if (sr) {
-                lvsrs.push([cc, sr]);
-            }
-        }
-        if (lvsrs.length > 0) {
-            srs.push([lv, lvsrs])
-        }
+  let srs = [];
+  for (let [lv, ccs] of lvCCs) {
+    let lvsrs = [];
+    for (let cc of ccs) {
+      sr = scoreRange(goalmin, goalmax, stepstat, cc);
+      if (cc < ccmin || cc > ccmax) {
+      } else if (sr) {
+        lvsrs.push([cc, sr]);
+      }
     }
-
-    let ccRows = [];
-    let lvRows = [];
-    for (let [lv, lvsrs] of srs) {
-        let lvscoremin = 0;
-        let lvscoremax = 10000000;
-        let ccbase = lvsrs["0"]["0"];
-        for ([cc, [scoremin, scoremax]] of lvsrs) {
-            lvscoremin = Math.max(lvscoremin, scoremin);
-            lvscoremax = Math.min(lvscoremax, scoremax);
-            let row = "\t\t<tr";
-            if (cc === ccbase) {
-                row = row + ' class="trlv"'
-            }
-            row += `><td>${cc}</td>`
-            row += `<td>${scoreDisp(scoremin)} &ndash; ${scoreDisp(scoremax)}</td>`
-            row += `<td>${scoreDisp(scoremax - scoremin)}</td></tr>`
-            ccRows.push(row);
-        }
-        if (lvscoremax > lvscoremin) {
-            row = `\t\t<tr class="trlv"><td>${lv}</td>`
-            row += `<td>${scoreDisp(lvscoremin)} &ndash; ${scoreDisp(lvscoremax)}</td>`
-            row += `<td>${scoreDisp(lvscoremax - lvscoremin)}</td></tr>`
-            lvRows.push(row);
-        }
+    if (lvsrs.length > 0) {
+      srs.push([lv, lvsrs]);
     }
-    scorerowscc.innerHTML = ccRows.join('\n');
-    scorerowslv.innerHTML = lvRows.join('\n');
+  }
 
-    if (goalmin > goalmax) {
-        errMsg.innerText = "步数范围无效";
-    } else if (ccmin > ccmax) {
-        errMsg.innerText = "等级范围无效";
-    } else if (srs.length === 0) {
-        let minsteps = (stepstat + 1) / 50 * 2.5;
-        let maxsteps = stepstat / 50 * (2.5 + 2.45 * (ccmax + 2) ** 0.5);
-        let reasons = [];
-        if (goalmin > maxsteps) {
-            reasons.push("使用具有更高 STEP 值的搭档")
-            if (ccmax < 12) {
-                reasons.push("扩展等级范围")
-            }
-            if (stamboost > 1 || fragboost > 1 || worldboost > 1 || mmrboost > 1) {
-                reasons.push("禁用或调低强化")
-            }
-        } else if (goalmax < minsteps) {
-            reasons.push("使用具有更低 STEP 值的搭档")
-        }
-        errstr = "步数范围超出, 请尝试下列方法:\n<ul>";
-        for (s of reasons) {
-            errstr += `\n\t<li>${s}</li>`
-        }
-        errMsg.innerHTML = errstr;
-    } else {
-        errMsg.innerText = "";
+  let ccRows = [];
+  let lvRows = [];
+  for (let [lv, lvsrs] of srs) {
+    let lvscoremin = 0;
+    let lvscoremax = 10000000;
+    let ccbase = lvsrs["0"]["0"];
+    for ([cc, [scoremin, scoremax]] of lvsrs) {
+      lvscoremin = Math.max(lvscoremin, scoremin);
+      lvscoremax = Math.min(lvscoremax, scoremax);
+      let row = "\t\t<tr";
+      if (cc === ccbase) {
+        row = row + ' class="trlv"';
+      }
+      row += `><td>${cc}</td>`;
+      row += `<td>${scoreDisp(scoremin)} &ndash; ${scoreDisp(scoremax)}</td>`;
+      row += `<td>${scoreDisp(scoremax - scoremin)}</td></tr>`;
+      ccRows.push(row);
     }
+    if (lvscoremax > lvscoremin) {
+      row = `\t\t<tr class="trlv"><td>${lv}</td>`;
+      row += `<td>${scoreDisp(lvscoremin)} &ndash; ${scoreDisp(
+        lvscoremax
+      )}</td>`;
+      row += `<td>${scoreDisp(lvscoremax - lvscoremin)}</td></tr>`;
+      lvRows.push(row);
+    }
+  }
+  scorerowscc.innerHTML = ccRows.join("\n");
+  scorerowslv.innerHTML = lvRows.join("\n");
+
+  if (goalmin > goalmax) {
+    errMsg.innerText = "步数范围无效";
+  } else if (ccmin > ccmax) {
+    errMsg.innerText = "等级范围无效";
+  } else if (srs.length === 0) {
+    let minsteps = ((stepstat + 1) / 50) * 2.5;
+    let maxsteps = (stepstat / 50) * (2.5 + 2.45 * (ccmax + 2) ** 0.5);
+    let reasons = [];
+    if (goalmin > maxsteps) {
+      reasons.push("使用具有更高 STEP 值的搭档");
+      if (ccmax < 12) {
+        reasons.push("扩展等级范围");
+      }
+      if (stamboost > 1 || fragboost > 1 || worldboost > 1 || mmrboost > 1) {
+        reasons.push("禁用或调低强化");
+      }
+    } else if (goalmax < minsteps) {
+      reasons.push("使用具有更低 STEP 值的搭档");
+    }
+    errstr = "步数范围超出, 请尝试下列方法:\n<ul>";
+    for (s of reasons) {
+      errstr += `\n\t<li>${s}</li>`;
+    }
+    errMsg.innerHTML = errstr;
+  } else {
+    errMsg.innerText = "";
+  }
 }
-
 
 // event listener to switch displayed table
 function scoresHandler(x) {
-    var tablesArray = Array.from(document.getElementsByClassName('scorestable'));
-    tablesArray.forEach(ele => {
-        if (ele.id === x) {
-            ele.classList.remove("hidden");
-        } else {
-            ele.classList.add("hidden");
-        }
-    })
+  var tablesArray = Array.from(document.getElementsByClassName("scorestable"));
+  tablesArray.forEach((ele) => {
+    if (ele.id === x) {
+      ele.classList.remove("hidden");
+    } else {
+      ele.classList.add("hidden");
+    }
+  });
 }
